e4da58e1a89fb37f5d9e11f2ea1655496823f239
charles-ison <isonc@oregonstate.edu>
2023-08-16 16:53:01 -0700

    Using model 1 iteration older if testing error increases.

--- tic_tac_toe/train_for_tic_tac_toe.py
+++ tic_tac_toe/train_for_tic_tac_toe.py
@@ -91,8 +91,8 @@ def build_data_loaders(results, player_1_roots, player_2_roots, batch_size, game
     return training_loader, testing_loader
 
 
-def get_best_player(trained_player, alt_player, trained_player_win_count, alt_player_win_count, log_best_player):
-    total_num_wins = trained_player_win_count + alt_player_win_count
+def get_best_player(trained_player, best_player, trained_player_win_count, best_player_win_count, log_best_player):
+    total_num_wins = trained_player_win_count + best_player_win_count
     trained_win_percentage = float(trained_player_win_count) / float(total_num_wins)
     print("Trained player win percentage: ", trained_win_percentage)
     if trained_win_percentage >= 0.55:
@@ -101,8 +101,8 @@ def get_best_player(trained_player, alt_player, trained_player_win_count, alt_pl
         return trained_player
     else:
         if log_best_player:
-            print("Using the alt player.")
-        return alt_player
+            print("Using the previous best player.")
+        return best_player
 
 
 def swap_players(players):
@@ -111,13 +111,13 @@ def swap_players(players):
     players[1] = temp
 
 
-def run_simulations(game, num_simulations, trained_player, alt_player, time_threshold, log_best_player):
-    trained_player_win_count, alt_player_win_count, tie_count = 0, 0, 0
+def run_simulations(game, num_simulations, trained_player, best_player, time_threshold, log_best_player):
+    trained_player_win_count, best_player_win_count, tie_count = 0, 0, 0
     results = []
     player_1_roots = []
     player_2_roots = []
     games = []
-    players = [trained_player, alt_player]
+    players = [trained_player, best_player]
     print("\nRunning simulations. . .")
     for game_num in range(num_simulations):
         new_game = copy.deepcopy(game)
@@ -131,15 +131,15 @@ def run_simulations(game, num_simulations, trained_player, alt_player, time_thre
         if result_num == get_player_num(game_num):
             trained_player_win_count += 1
         elif result_num == get_player_num(game_num + 1):
-            alt_player_win_count += 1
+            best_player_win_count += 1
         elif result_num == 0:
             tie_count += 1
 
     print("\nNumber of trained player wins: " + str(trained_player_win_count))
-    print("Number of alt player wins: " + str(alt_player_win_count))
+    print("Number of previous best player wins: " + str(best_player_win_count))
     print("Number of ties: " + str(tie_count))
 
-    best_player = get_best_player(trained_player, alt_player, trained_player_win_count, alt_player_win_count, log_best_player)
+    best_player = get_best_player(trained_player, best_player, trained_player_win_count, best_player_win_count, log_best_player)
     return best_player, results, player_1_roots, player_2_roots, games
 
 def test(model, testing_loader, device, criterion):
@@ -157,13 +157,15 @@ def test(model, testing_loader, device, criterion):
     return total_loss
 
 
-def train(model, optimizer, training_loader, testing_loader, device, criterion, epochs):
+def train_model(model, training_loader, testing_loader, device, criterion, epochs, lr):
     print("\nTraining. . .")
+    optimizer = optim.Adam(model.parameters(), lr=lr)
     model.train()
     old_testing_loss = None
     for epoch in range(epochs):
         print("Epoch: ", epoch)
-        total_loss = 0.0
+        total_training_loss = 0.0
+        old_model = copy.deepcopy(model)
         for batch in training_loader:
             data, prob, value = batch['data'].to(device), batch['label'][0].to(device), batch['label'][1].to(device)
             optimizer.zero_grad()
@@ -175,16 +177,17 @@ def train(model, optimizer, training_loader, testing_loader, device, criterion, 
             predicted_value = predicted_value.flatten()
 
             loss = criterion.calculate(predicted_prob, prob, predicted_value, value)
-            total_loss += loss.item()
+            total_training_loss += loss.item()
             loss.backward()
             optimizer.step()
 
         testing_loss = test(model, testing_loader, device, criterion)
-        print("Training Loss: " + str(total_loss) + " and Testing Loss: " + str(testing_loss))
+        print("Training Loss: " + str(total_training_loss) + " and Testing Loss: " + str(testing_loss))
         if old_testing_loss is not None and testing_loss >= old_testing_loss:
-            print("Old testing loss less than current loss, stopping training.")
-            break
+            print("Testing loss increasing, using model from one iteration earlier.")
+            return old_model
         old_testing_loss = testing_loss
+    return model
 
 
 def save_best_player(best_player):
@@ -193,29 +196,25 @@ def save_best_player(best_player):
 
 def run_reinforcement(num_checkpoints, game, device, criterion, num_simulations, epochs, time_threshold, lr):
     print("Running Reinforcement Learning")
-    trained_player = Player(PlayerType.Untrained_MCTS_CNN)
-    alt_player = Player(PlayerType.Untrained_MCTS_CNN)
+    player_1, player_2 = Player(PlayerType.Untrained_MCTS_CNN), Player(PlayerType.Untrained_MCTS_CNN)
+    best_player, results, player_1_roots, player_2_roots, games = run_simulations(game, num_simulations, player_1, player_2, time_threshold, True)
 
     for step in range(num_checkpoints):
         print("\nCheckpoint number: ", step)
-        best_player, results, player_1_roots, player_2_roots, games = run_simulations(game, num_simulations, trained_player, alt_player, time_threshold, True)
-        save_best_player(best_player)
-
-        if step % 10 == 0 and step != 0:
+        if step % 10 == 0:
             print("\nComparing performance to untrained CNN")
             run_simulations(game, num_simulations, best_player, Player(PlayerType.Untrained_MCTS_CNN), time_threshold, False)
             print("\nComparing performance to Pure MCTS")
             run_simulations(game, num_simulations, best_player, Player(PlayerType.Pure_MCTS), time_threshold, False)
 
-        trained_player, alt_player = best_player, copy.deepcopy(best_player)
-        optimizer = optim.Adam(trained_player.alpha_go_zero_lite.cnn.parameters(), lr=lr)
-        alpha_go_zero_lite = trained_player.alpha_go_zero_lite
+        training_loader, testing_loader = build_data_loaders(results, player_1_roots, player_2_roots, batch_size, games, best_player.alpha_go_zero_lite)
+        trained_player = copy.deepcopy(best_player)
+        model = trained_player.alpha_go_zero_lite.cnn
+        trained_model = train_model(model, training_loader, testing_loader, device, criterion, epochs, lr)
+        trained_player.alpha_go_zero_lite.cnn = trained_model
 
-        training_loader, testing_loader = build_data_loaders(results, player_1_roots, player_2_roots, batch_size, games, alpha_go_zero_lite)
-        train(trained_player.alpha_go_zero_lite.cnn, optimizer, training_loader, testing_loader, device, criterion, epochs)
-
-    best_player, _, _, _, _ = run_simulations(game, num_simulations, trained_player, alt_player, time_threshold)
-    save_best_player(best_player)
+        best_player, results, player_1_roots, player_2_roots, games = run_simulations(game, num_simulations, trained_player, best_player, time_threshold, True)
+        save_best_player(best_player)
 
 
 lr = 0.0001
